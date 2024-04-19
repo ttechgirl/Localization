2f3fa2b0b2578cb761fc8d024825ba77f0eca3c2
ttechgirl <abiodunayisat2@gmail.com>
2024-04-19 08:45:40 +0100

    Add project files.

---
+++ Localization/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Localization.Models;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Localization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly LanguageServices _localization;
+        public HomeController( LanguageServices localization)
+        {
+            _localization = localization;
+        }
+        #region Localization
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.WelcomeMessage = _localization.Getkey("Welcome").Value;
+            //get culture information
+            var currentCulture = Thread.CurrentThread.CurrentUICulture.Name;
+            
+            return View();
+        }
+
+        public IActionResult ChangeLanguage(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions()
+            {
+                Expires = DateTimeOffset.UtcNow.AddYears(1)
+            });
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+        #endregion
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Localization/LanguageServices.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Localization
+{
+    public class LanguageServices
+    {
+        private readonly IStringLocalizer _localizer;
+        public LanguageServices(IStringLocalizerFactory factory)
+        {
+            var type = typeof(SharedResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            _localizer = factory.Create("SharedResource", assemblyName.Name); // §REVIEW_DJE: "SharedResource" or "ShareResource"
+        }
+        public LocalizedString Getkey(string key)
+        {
+            return _localizer[key];
+        }
+    }
+
+    /// <summary>
+    /// Dummy class to group shared resources
+    /// </summary>
+    public class SharedResource { }
+
+}
